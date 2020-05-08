dff6cb34088bd98233751a0b1500f65a1526deba
Ubuntu <ubuntu@ip-172-31-42-233.ap-northeast-1.compute.internal>
2020-05-08 06:41:46 +0000

    edit home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "FLYB(フライビー）"
     if page_title.empty?
-      base_title
+      base_title + "｜メンズコスメの口コミ"
     else
       page_title + " | " + base_title
     end
